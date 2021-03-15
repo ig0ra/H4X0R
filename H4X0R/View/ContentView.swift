70a9cefc9e543ce1d54c7c667faebfbd95f2b2f6
ig0ra <51413314+ig0ra@users.noreply.github.com>
2021-03-15 19:47:53 +0200

    Initial Commit

---
+++ H4X0R/View/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Admin on 15.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear{
+            self.networkManager.fetchData()}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Hello"),
+//    Post(id: "3", title: "Hello"),
+//]
